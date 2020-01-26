270b5ffa7600087aff60235a9dc8d827629cf004
Nelson <nbritob@hotmail.com>
2020-01-26 23:21:50 +0000

    adds divide by 0

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -17,6 +17,7 @@ describe('calculator functionality', function() {
   })
 
   it('should update the display with the arithmetic operation result', function() {
+    running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     element(by.css('#operator_add')).click();
     element(by.css('#number2')).click();
@@ -24,7 +25,8 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('4');
   })
 
-  it('is should chain multiple operations together', function() {
+  it('should chain multiple operations together', function() {
+    running_total = element(by.css('#running_total'))
     element(by.css('#number3')).click();
     element(by.css('#operator_add')).click();
     element(by.css('#number3')).click();
@@ -32,7 +34,59 @@ describe('calculator functionality', function() {
     element(by.css('#number3')).click();
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('9');
+  })
 
+  it('should be able to output a negative number', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-1');
+
+  })
+
+  it('should be able to output decimals', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number6')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('0.5');
+  })
+
+  it('should be able to handle very large numbers', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('140167369669070240');
+  })
+
+  it('should give a message when trying to divide by 0', function() {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number6')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('Cannot divide by 0');
   })
 
 });
